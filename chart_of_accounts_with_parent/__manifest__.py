148edb573018104a58a6e85e966a474a625d9173
Select Solutions <37082747+Selectsolution@users.noreply.github.com>
2018-10-20 01:49:19 +0200

    first publish

---
+++ chart_of_accounts_with_parent/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Chart of Accounts with Parent and Levels and Auto Account Code",
+    'version': '11.0.1.0',
+    'author': 'Select Solutions',
+    'company': 'Select Solutions',
+    'website': "http://www.select-online.net",
+    'category': 'Accounting',
+    'summary': """Chart of Accounts As Leveling Accounting with parent and child """,
+    'description': """Account Chart As Egypt Accounting with parent and child ,Also Accounts Levels and with Auto Account Code """,
+    'depends': ['base','account'],
+    'data': [
+        'views/views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
